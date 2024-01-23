991907ea229a94530e0d0660202ffbc157c23869
OlehBrov <o.dudnyk85@gmail.com>
2024-01-23 13:15:25 +0200

    basename added

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+    <BrowserRouter basename="/glivera">
       <App />
     </BrowserRouter>
   </React.StrictMode>
