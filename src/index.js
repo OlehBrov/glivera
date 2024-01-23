fcb12fa6bd295e3da42ad1c7407250440fb99100
OlehBrov <o.dudnyk85@gmail.com>
2024-01-23 13:26:24 +0200

    change to hashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/glivera">
+    <HashRouter basename="/glivera">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
